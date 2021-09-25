ab6637a0dc49309ef97d2c0a3b6ad33efae01d66
alifr <frank.ali@nih.gov>
2021-09-25 16:25:41 -0400

    adding attrib inputs and dice roller buttons adding 3d-dice packages adding sass component support

--- src/Components/CharacterSheet/index.js
+++ src/Components/CharacterSheet/index.js
@@ -3,7 +3,7 @@ import TalentList from "../TalentList";
 import LevelsTable from "./LevelsTable";
 import PresetsSelector from "./PresetsSelector";
 import DisadSelector from "./DisadSelector";
-import Attributes from "./Attributes";
+import Attributes from "./attributes/Attributes";
 import {
   aspectData,
   dataDisads,
--- src/Data/dataAttributes.js
+++ src/Data/dataAttributes.js
@@ -1,62 +1,61 @@
-const dataAttributes = [
-    { 
-       'name' : 'strength',
-       'roll'  : 10,
-       'min' : 3,
-       'max' : 18,
-       'mod' : 0,
-       'total' : 0,
-       'bonus': 0,
-       
-   },
-   { 
-       'name' : 'dexterity',
-       'roll'  : 10,
-       'min' : 3,
-       'max' : 18,
-       'mod' : 0,
-       'total' : 0,
-       'bonus': 0,
-   },
-    { 
-       'name' : 'constitution',
-       'roll'  : 10,
-       'min' : 3,
-       'max' : 18,
-       'mod' : 0,
-       'total' : 0,
-       'bonus': 0,
-   },
-   { 
-       'name' : 'intelligence',
-       'roll'  : 10,
-       'min' : 3,
-       'max' : 18,
-       'mod' : 0,
-       'total' : 0,
-       'bonus': 0,
-   },
-    { 
-       'name' : 'wisdom',
-       'roll'  : 10,
-       'min' : 3,
-       'max' : 18,
-       'mod' : 0,
-       'total' : 0,
-       'bonus': 0,
-   },
-  { 
-       'name' : 'charisma',
-       'roll'  : 10,
-       'min' : 3,
-       'max' : 18,
-       'mod' : 0,
-       'total' : 0,
-       'bonus': 0,
-   }
-];
+const dataAttributes = {
+	strength:   { 
+		'name' : 'strength',
+		'roll'  : 10,
+		'min' : 3,
+		'max' : 18,
+		'mod' : 0,
+		'total' : 0,
+		'bonus': 0,
+	},
+	dexterity:  { 
+		'name' : 'dexterity',
+		'roll'  : 10,
+		'min' : 3,
+		'max' : 18,
+		'mod' : 0,
+		'total' : 0,
+		'bonus': 0,
+	},
+	constitution:   { 
+		'name' : 'constitution',
+		'roll'  : 10,
+		'min' : 3,
+		'max' : 18,
+		'mod' : 0,
+		'total' : 0,
+		'bonus': 0,
+	},
+	intelligence:  { 
+		'name' : 'intelligence',
+		'roll'  : 10,
+		'min' : 3,
+		'max' : 18,
+		'mod' : 0,
+		'total' : 0,
+		'bonus': 0,
+	},
+	wisdom: { 
+		'name' : 'wisdom',
+		'roll'  : 10,
+		'min' : 3,
+		'max' : 18,
+		'mod' : 0,
+		'total' : 0,
+		'bonus': 0,
+	},
+	charisma: { 
+		'name' : 'charisma',
+		'roll'  : 10,
+		'min' : 3,
+		'max' : 18,
+		'mod' : 0,
+		'total' : 0,
+		'bonus': 0,
+	}
+};
 
 
 export {
-    dataAttributes
+	dataAttributes
 }
