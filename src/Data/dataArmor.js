5c437585445484a770a2ed6ae61e865966730e9c
Michael Fitzgerald <mfitz@mac.com>
2021-09-27 19:44:19 -0400

    armor data

---
+++ src/Data/dataArmor.js
@@ -0,0 +1,69 @@
+const armorData = [
+  {
+    armor: "none",
+    ac: 0,
+    penalty: "-",
+  },
+  {
+    armor: "Leather",
+    ac: 2,
+    penalty: "-",
+  },
+  {
+    armor: "Padded",
+    ac: 2,
+    penalty: "-",
+  },
+  {
+    armor: "Ring mail",
+    ac: 3,
+    penalty: -2,
+  },
+  {
+    armor: "Studded leather",
+    ac: 3,
+    penalty: -2,
+  },
+  {
+    armor: "Cuir-bouilli",
+    ac: 3,
+    penalty: -2,
+  },
+  {
+    armor: "Scale mail",
+    ac: 5,
+    penalty: -3,
+  },
+  {
+    armor: "chain mail",
+    ac: 5,
+    penalty: -3,
+  },
+  {
+    armor: "Brigandine",
+    ac: 5,
+    penalty: -3,
+  },
+  {
+    armor: "Splint mail",
+    ac: 6,
+    penalty: -3,
+  },
+  {
+    armor: "Banded mail",
+    ac: 6,
+    penalty: -3,
+  },
+  {
+    armor: "Plate mail",
+    ac: 6,
+    penalty: -3,
+  },
+  {
+    armor: "Full plate mail",
+    ac: 8,
+    penalty: -5,
+  },
+];
+
+export { armorData };
