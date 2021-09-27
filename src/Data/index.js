a4a53ae7f06eaaf0587367c2725c69a3ab26ad8c
frankieali <frankieali4@gmail.com>
2021-09-27 10:42:59 -0400

    adding attribute modifiers

--- src/Data/index.js
+++ src/Data/index.js
@@ -1,8 +1,8 @@
-import { dataAttributes } from './dataAttributes';
+import { dataAttributes, dataAttributesMods } from './dataAttributes';
 import { levelsData } from './dataLevels';
 import { talentData } from './dataTalents';
 import { raceData } from './dataRaces';
 import { aspectData } from './dataAspects';
 import { presetData } from './dataPresets';
 import { dataDisads } from './dataDisads';
-export { aspectData, dataAttributes, dataDisads, levelsData, presetData, talentData, raceData };
+export { aspectData, dataAttributes, dataAttributesMods, dataDisads, levelsData, presetData, talentData, raceData };
