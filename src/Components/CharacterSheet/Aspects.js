fd30a1cac69e86b65241638d6bd3cd17dd2e97e2
Michael Fitzgerald <mfitz@mac.com>
2021-09-29 09:28:19 -0400

    start on new class/aspect picker

--- src/Components/CharacterSheet/Aspects.js
+++ src/Components/CharacterSheet/Aspects.js
@@ -1,25 +1,69 @@
 import React, { useState } from "react";
+import ReactModal from "react-modal";
 import { aspectData } from "../../Data";
 
 const Aspects = (props) => {
+  const [aspectModalOpen, setAspectModalOpen] = useState(false);
+  const toggleAspectModal = () => {
+    setAspectModalOpen(!aspectModalOpen);
+  };
+
   return (
     <div className="attributes">
       {/*  ------- CLASS ------ */}
-      {aspectData.map((i) => (
-        <span key={i.name} className="checkbox-wrapper">
-          <input
-            type="radio"
-            name="class"
-            value={i.id}
-            id={i.name}
-            checked={props.character.aspect === i.name}
-            onClick={props.handleCharAspect}
-          ></input>
-          <label htmlFor={i.name} className={i.name}>
-            {i.name}
-          </label>
-        </span>
-      ))}
+      <label>
+        <button className="button" onClick={toggleAspectModal}>
+          Class
+        </button>
+        <br />
+        <span className="label">Class</span>
+      </label>
+
+      {/*  ------- MODAL WITH CLASS PICKER ------ */}
+      <ReactModal
+        isOpen={aspectModalOpen}
+        onRequestClose={toggleAspectModal}
+        contentLabel="Choose Class"
+        className="modal"
+        overlayClassName="modal-overlay"
+        ariaHideApp={false}
+      >
+        <div className="modal__header">
+          <button
+            className="button modal__header-button"
+            onClick={toggleAspectModal}
+          >
+            close
+          </button>
+        </div>
+
+        <div className="modal__body">
+          <h2>Choose Class</h2>
+          {aspectData.map((i) => (
+            <span key={i.name} className="checkbox-wrapper">
+              <input
+                type="radio"
+                name="class"
+                value={i.id}
+                id={i.name}
+                checked={props.character.aspect === i.name}
+                onClick={props.handleCharAspect}
+              ></input>
+              <label htmlFor={i.name} className={i.name}>
+                {i.name}
+              </label>
+            </span>
+          ))}
+        </div>
+        <div className="modal__footer">
+          <button
+            className="button modal__header-button"
+            onClick={toggleAspectModal}
+          >
+            close
+          </button>
+        </div>
+      </ReactModal>
     </div>
   );
 };
