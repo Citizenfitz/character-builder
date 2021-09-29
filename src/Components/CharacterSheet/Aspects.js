e620a9507f54426ac3f51a4617997a0836af37bc
Michael Fitzgerald <mfitz@mac.com>
2021-09-29 12:50:42 -0400

    crude style fixes

--- src/Components/CharacterSheet/Aspects.js
+++ src/Components/CharacterSheet/Aspects.js
@@ -51,28 +51,37 @@ const Aspects = (props) => {
           <p className="ut-margin-top-0">
             A character's class is their main "mode" or approach to challenges.
             There are four arranged such that each has two adjacent classes
-            <i> (those to the left and right)</i> and one opposing class
+            <i> (those to the left and right) </i> and one opposing class
             <i> (the one directly across)</i>. A character's talents are
             strongest within their class, middling in adjacent classes (1/2
             their level), and poor in opposing ones (1/4 their level). Each
             class has one or more starting talents gains additional talents as
             they advance in level.
           </p>
-          {aspectData.map((i) => (
-            <span key={i.name} className="checkbox-wrapper">
-              <input
-                type="radio"
-                name="class"
-                value={i.id}
-                id={i.name}
-                checked={props.character.aspect === i.name}
-                onClick={props.handleCharAspect}
-              ></input>
-              <label htmlFor={i.name} className={i.name}>
-                {i.name}
-              </label>
-            </span>
-          ))}
+          <ul className="aspect-radio-set">
+            {aspectData.map((i) => (
+              <li key={i.name} className="aspect-radio-set__item">
+                <input
+                  type="radio"
+                  name="class"
+                  value={i.id}
+                  id={i.name}
+                  checked={props.character.aspect === i.name}
+                  onClick={props.handleCharAspect}
+                  className="aspect-radio-button"
+                ></input>
+                <label
+                  htmlFor={i.name}
+                  className={`aspect-radio-label aspect-radio-label--${i.name}`}
+                >
+                  <span
+                    className={`aspect-icon aspect-icon--in-button aspect-icon--${i.name}`}
+                  ></span>
+                  {i.name}
+                </label>
+              </li>
+            ))}
+          </ul>
         </div>
         <div className="modal__footer">
           <button
