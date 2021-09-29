242fc02e25f255dcafdc94cfac846f821d983789
Michael Fitzgerald <mfitz@mac.com>
2021-09-29 09:53:41 -0400

    more basic work

--- src/Components/CharacterSheet/Aspects.js
+++ src/Components/CharacterSheet/Aspects.js
@@ -29,16 +29,27 @@ const Aspects = (props) => {
         ariaHideApp={false}
       >
         <div className="modal__header">
+          <h2 className="modal__h2">Choose a Class</h2>
           <button
             className="button modal__header-button"
             onClick={toggleAspectModal}
+            aria-label="Close modal"
           >
-            close
+            X
           </button>
         </div>
 
         <div className="modal__body">
-          <h2>Choose Class</h2>
+          <p className="ut-margin-top-0">
+            A character's class is their main "mode" or approach to challenges.
+            There are four arranged such that each has two adjacent classes
+            <i> (those to the left and right)</i> and one opposing class
+            <i>(the one directly across)</i>. A character's talents are
+            strongest within their class, middling in adjacent classes (1/2
+            their level), and poor in opposing ones (1/4 their level). Each
+            class has one or more starting talents gains additional talents as
+            they advance in level.
+          </p>
           {aspectData.map((i) => (
             <span key={i.name} className="checkbox-wrapper">
               <input
@@ -56,10 +67,7 @@ const Aspects = (props) => {
           ))}
         </div>
         <div className="modal__footer">
-          <button
-            className="button modal__header-button"
-            onClick={toggleAspectModal}
-          >
+          <button className="button" onClick={toggleAspectModal}>
             close
           </button>
         </div>
