8ec5ecf18df28bf40f552a9e1bd0bf3fa3255788
frankieali <frankieali4@gmail.com>
2021-10-01 11:09:00 -0400

    add legal attribution for game-icons.net

--- src/Components/CharacterSheet/Profile.js
+++ src/Components/CharacterSheet/Profile.js
@@ -56,6 +56,7 @@ const ProfilePic = (props) => {
             </button>
           ))}
         </div>
+        <div className="attribution">Avatar icons supplied by <a href="https://game-icons.net/" target="_blank" rel="noreferrer">game-icons.net</a> and licensed under <a href="https://creativecommons.org/licenses/by/3.0/legalcode" target="_blank" rel="noreferrer">CC BY 3.0</a></div>
       </Modal>
     </div>
   )
--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -4,6 +4,7 @@ const Footer = () => (
   <footer className="footer" id="footer">
 
   FOOTER
+  <div className="attribution">Avatar icons supplied by <a href="https://game-icons.net/" target="_blank" rel="noreferrer">game-icons.net</a> and licensed under <a href="https://creativecommons.org/licenses/by/3.0/legalcode" target="_blank" rel="noreferrer">CC BY 3.0</a></div>
 
   </footer>
 );
