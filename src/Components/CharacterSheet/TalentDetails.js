2c46a5c73a61b78ac01899aab5eecb948f44d96a
Michael Fitzgerald <mfitz@mac.com>
2025-02-09 21:27:21 -0500

    fixed talent details to only show talents for their current level

--- src/Components/CharacterSheet/TalentDetails.js
+++ src/Components/CharacterSheet/TalentDetails.js
@@ -3,10 +3,23 @@ import { talentData } from "../../Data";
 import RenderTalent from "../Utilities/RenderTalent";
 
 const TalentDetails = ({ character }) => {
+  // Helper function to filter talents based on level requirements
+  const getAvailableTalents = () => {
+    const talents = { ...character.talents };
+
+    // Remove talents that shouldn't be shown based on character level
+    if (character.level < 3) delete talents.talentLevel3;
+    if (character.level < 5) delete talents.talentLevel5;
+    if (character.level < 7) delete talents.talentLevel7;
+    if (character.level < 9) delete talents.talentLevel9;
+
+    return Object.values(talents);
+  };
+
   return (
     <div>
       {/* Talent Descriptions */}
-      {Object.values(character.talents)
+      {getAvailableTalents()
         .filter((talent) => talent !== "choose")
         .map((talentName) => {
           const talent = talentData.find((t) => t.name === talentName);
